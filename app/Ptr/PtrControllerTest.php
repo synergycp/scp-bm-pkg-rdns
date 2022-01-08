453a844137832bf6f8e3cd41ef9a8dca45e5c7fe
Jacob <jacob@synergycp.com>
2022-01-07 18:07:51 -0700

    changed error code thrown by invlaid ptr, updated tests for laravel 5.4

--- app/Ptr/PtrControllerTest.php
+++ app/Ptr/PtrControllerTest.php
@@ -6,7 +6,7 @@ use App\Entity\Entity;
 use App\Ip\IpAddressRangeContract;
 use App\Ip\IpAddressV4;
 use App\Server\Port\ServerPortWithEntitiesTestNetwork;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Foundation\Testing\TestResponse;
 use Packages\Rdns\App\RdnsTestCase;
 
 class PtrControllerTest extends RdnsTestCase {
@@ -164,11 +164,11 @@ class PtrControllerTest extends RdnsTestCase {
       $ip = "9.9.9.9";
       $this->tryCreate(['ip' => $ip]);
       $this->assertResponseStatus(409);
-      $this->assertMessageContains('Invalid IP, IP is not associated with Ptr');
+      $this->assertMessageContains("Invalid PTR, Please ensure that test_create has an A or AAAA DNS record to 9.9.9.9");
     });
   }
 
-  protected function tryCreate(array $data = []): JsonResponse {
+  protected function tryCreate(array $data = []): TestResponse {
     return $this->post($this->url(), $data + ['ptr' => 'test_create']);
   }
 
@@ -180,7 +180,7 @@ class PtrControllerTest extends RdnsTestCase {
     return (string) $this->ipRange->end();
   }
 
-  protected function canCreateInsideEntityRange(): JsonResponse {
+  protected function canCreateInsideEntityRange(): TestResponse {
     $this->mockDns('test_create', '1.1.1.3', 1);
     $this->expectsEvents(Events\PtrCreated::class);
 
@@ -192,7 +192,7 @@ class PtrControllerTest extends RdnsTestCase {
     return $result;
   }
 
-  protected function canCreateOutsideEntityRange(): JsonResponse {
+  protected function canCreateOutsideEntityRange(): TestResponse {
     $this->mockDns('test_create', '5.5.5.5', 1);
     $this->expectsEvents(Events\PtrCreated::class);
 
@@ -230,20 +230,6 @@ class PtrControllerTest extends RdnsTestCase {
     ]);
   }
 
-  private function dontSeeInternalPTR(): void {
-    $this->dontSeeJson([
-      'ptr' => 'test_int_ptr_name',
-      'ip' => $this->startEntityRange(),
-    ]);
-  }
-
-  private function dontSeeExternalPTR(): void {
-    $this->dontSeeJson([
-      'ptr' => 'test_ext_ptr_name',
-      'ip' => '8.8.8.8',
-    ]);
-  }
-
   protected function canReadOutsideEntityRange(): void {
     $this->get($this->url());
     $this->assertResponseOk();
@@ -255,38 +241,38 @@ class PtrControllerTest extends RdnsTestCase {
   }
 
   protected function cannotReadInsideEntityRange(): void {
-    $this->get($this->url());
-    $this->assertResponseOk();
-    $this->dontSeeInternalPTR();
+    $this->get($this->url())
+    ->assertSuccessful()
+    ->assertJson(['data'=>[]]);
 
     $this->get($this->url() . "/" . $this->ptr->id);
     $this->assertResponseStatusOneOf([404, 403]);
   }
 
   protected function cannotReadOutsideEntityRange(): void {
-    $this->get($this->url());
-    $this->assertResponseOk();
-    $this->dontSeeExternalPTR();
+    $this->get($this->url())
+    ->assertSuccessful()
+    ->assertJson(['data'=>[]]);
 
     $this->get($this->url() . "/" . $this->externalPtr->id);
     $this->assertResponseStatusOneOf([404, 403]);
   }
 
-  protected function cannotCreateInsideEntityRange(): JsonResponse {
+  protected function cannotCreateInsideEntityRange(): TestResponse {
     $this->mockDns('test_create', '1.1.1.3', 0);
     $resp = $this->tryCreate(['ip' => $this->endEntityRange()]);
     $this->assertResponseStatusOneOf([404, 403]);
     return $resp;
   }
 
-  protected function cannotCreateOutsideEntityRange(): JsonResponse {
+  protected function cannotCreateOutsideEntityRange(): TestResponse {
     $this->mockDns('test_create', '5.5.5.5', 0);
     $resp = $this->tryCreate(['ip' => '5.5.5.5']);
     $this->assertResponseStatusOneOf([404, 403]);
     return $resp;
   }
 
-  protected function cannotUpdateInsideEntityRange(): JsonResponse {
+  protected function cannotUpdateInsideEntityRange(): TestResponse {
     $this->mockDns('test_edit', '1.1.1.1', 0);
     $resp = $this->patch($this->url($this->ptr), [
       'ptr' => 'test_edit',
@@ -295,7 +281,7 @@ class PtrControllerTest extends RdnsTestCase {
     return $resp;
   }
 
-  protected function cannotUpdateOutsideEntityRange(): JsonResponse {
+  protected function cannotUpdateOutsideEntityRange(): TestResponse {
     $this->mockDns('test_edit', '8.8.8.8', 0);
     $resp = $this->patch($this->url($this->externalPtr), [
       'ptr' => 'test_edit',
@@ -304,7 +290,7 @@ class PtrControllerTest extends RdnsTestCase {
     return $resp;
   }
 
-  protected function canUpdateInsideEntityRange(): JsonResponse {
+  protected function canUpdateInsideEntityRange(): TestResponse {
     $this->expectsEvents(Events\PtrPtrUpdated::class);
     $this->mockDns('test_edit', '1.1.1.1', 1);  
     $resp = $this->patch($this->url($this->ptr), [
@@ -316,7 +302,7 @@ class PtrControllerTest extends RdnsTestCase {
     return $resp;
   }
 
-  protected function canUpdateOutsideEntityRange(): JsonResponse {
+  protected function canUpdateOutsideEntityRange(): TestResponse {
     $this->expectsEvents(Events\PtrPtrUpdated::class);
     $this->mockDns('test_edit', '8.8.8.8', 1);
     $resp = $this->patch($this->url($this->externalPtr), [
@@ -328,14 +314,14 @@ class PtrControllerTest extends RdnsTestCase {
     return $resp;
   }
 
-  protected function canDeleteInsideEntityRange(): JsonResponse {
+  protected function canDeleteInsideEntityRange(): TestResponse {
     $this->expectsEvents(Events\PtrDeleted::class);
     $resp = $this->delete($this->url($this->ptr));
     $this->assertResponseOk();
     return $resp;
   }
 
-  protected function canDeleteOutsideEntityRange(): JsonResponse {
+  protected function canDeleteOutsideEntityRange(): TestResponse {
     $this->expectsEvents(Events\PtrDeleted::class);
 
     $resp = $this->delete($this->url($this->externalPtr));
@@ -343,13 +329,13 @@ class PtrControllerTest extends RdnsTestCase {
     return $resp;
   }
 
-  protected function cannotDeleteInsideEntityRange(): JsonResponse {
+  protected function cannotDeleteInsideEntityRange(): TestResponse {
     $resp = $this->delete($this->url($this->ptr));
     $this->assertResponseStatusOneOf([404, 403]);
     return $resp;
   }
 
-  protected function cannotDeleteOutsideEntityRange(): JsonResponse {
+  protected function cannotDeleteOutsideEntityRange(): TestResponse {
     $resp = $this->delete($this->url($this->externalPtr));
     $this->assertResponseStatusOneOf([404, 403]);
     return $resp;
